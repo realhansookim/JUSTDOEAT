054a59d94a66989cd3c83b2c58567663144c7786
affkdy <affkdy@gmail.com>
2023-01-10 16:47:19 +0900

    기본

---
+++ src/main/java/com/team5/justdoeat/menu/entity/MenuCategoryEntity.java
@@ -0,0 +1,27 @@
+package com.team5.justdoeat.menu.entity;
+
+import org.hibernate.annotations.DynamicInsert;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "menu_category")
+@DynamicInsert
+@Builder
+public class MenuCategoryEntity {
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "mca_seq") private Long mcaSeq;
+  @Column(name = "mca_name")private  String mcaName;
+}
