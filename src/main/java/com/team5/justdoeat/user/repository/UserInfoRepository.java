9ae8cfc0bc5c3d18055d11517676d00613af5291
affkdy <120349008+affkdy@users.noreply.github.com>
2023-01-10 17:53:41 +0900

    Update UserInfoRepository.java

--- src/main/java/com/team5/justdoeat/user/repository/UserInfoRepository.java
+++ src/main/java/com/team5/justdoeat/user/repository/UserInfoRepository.java
@@ -15,12 +15,4 @@ public interface UserInfoRepository extends JpaRepository<UserInfoEntity,Long> {
  public UserInfoEntity findByUiNameAndUiEmail(String name, String email);
  public Integer countByUiId(String uiId);
  public UserInfoEntity findByUiIdAndUiPwd(String uiid, String uipwd);
-=======
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.team5.justdoeat.user.entity.UserInfoEntity;
-
-public interface UserInfoRepository extends JpaRepository<UserInfoEntity,Long>{
-  UserInfoEntity findByUiSeq(Long uiSeq);
->>>>>>> d316d98c7a34e68e052d44a72b0670a3dfb5958a
 }
