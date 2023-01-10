d67537a24567fb4a8d48792061972f6b75e03ae9
realhansookim <kimhansu115@gmail.com>
2023-01-10 16:06:30 +0900

    asd

---
+++ src/test/java/com/team5/justdoeat/JustdoeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team5.justdoeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustdoeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
