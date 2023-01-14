6779f23ad26d6dcaa929b27c43cf0a50603947c3
DHkim8362 <ehdgusdl8362@gmail.com>
2023-01-14 14:39:31 +0900

    20230114_143900

--- src/main/java/com/team5/justdoeat/store/repository/StoreInfoRepository.java
+++ src/main/java/com/team5/justdoeat/store/repository/StoreInfoRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.team5.justdoeat.store.entity.StoreInfoEntity;
@@ -13,4 +14,7 @@ public interface StoreInfoRepository extends JpaRepository<StoreInfoEntity, Long
     @Query(value = "select a from StoreInfoEntity a")
     Page<StoreInfoEntity> getStoreInfoList(Pageable Pageable);
 
+    @Query(value = "select a from StoreInfoEntity a where a.siName like %:keyword%")
+    Page<StoreInfoEntity> findBySiName(Pageable pageable, @Param("keyword") String keyword);
+    
 }
--- src/main/java/com/team5/justdoeat/store/service/StoreService.java
+++ src/main/java/com/team5/justdoeat/store/service/StoreService.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import com.team5.justdoeat.store.entity.StoreCategoryInfoEntity;
 import com.team5.justdoeat.store.entity.StoreDetailEntity;
 import com.team5.justdoeat.store.entity.StoreInfoEntity;
 import com.team5.justdoeat.store.repository.StoreCategoryConnectRepository;
@@ -31,6 +32,7 @@ public class StoreService {
         return resultMap;
     }
 
+    // 가게 기본정보 검색할때 정보 표시 기능
     public Map<String, Object> getStoreInfoList(Pageable pageable) {
         Page<StoreInfoEntity> page = sInfoRepo.getStoreInfoList(pageable);
         Map<String, Object> resultMap = new LinkedHashMap<String, Object>();
@@ -41,6 +43,7 @@ public class StoreService {
         return resultMap;
     }
 
+    // 가게 상세정보 검색할때 정보 표시 기능
     public Map<String, Object> getStoreDetailList(Pageable pageable) {
         Page<StoreDetailEntity> page = sDetailRepo.getStoreDetailList(pageable);
         Map<String, Object> resultMap = new LinkedHashMap<String, Object>();
@@ -51,21 +54,49 @@ public class StoreService {
         return resultMap;
     }
 
+    // 가게 기본정보 추가할때 메시지 표시 기능
     public Map<String, Object> addStoreInfo(StoreInfoEntity data) {
         Map<String, Object> map = new LinkedHashMap<String, Object>();
         sInfoRepo.save(data);
         map.put("status", true);
-        map.put("message", "데이터가 저장되었습니다.");
+        map.put("message", "가게 기본정보가 추가 되었습니다.");
         map.put("code", HttpStatus.CREATED);
         return map;
     }
 
+    // 가게 상세정보 추가할떄 메시지 표시 기능
     public Map<String, Object> addStoreDetail(StoreDetailEntity data) {
         Map<String, Object> map = new LinkedHashMap<String, Object>();
         sDetailRepo.save(data);
         map.put("status", true);
-        map.put("message", "데이터가 저장되었습니다.");
+        map.put("message", "가게 상세정보가 추가 되었습니다.");
         map.put("code", HttpStatus.CREATED);
         return map;
+    }
+
+    // 가게 카테고리 추가할때 메시지 표시 기능
+    public Map<String, Object> addStoreCategory(StoreCategoryInfoEntity data) {
+        Map<String, Object> map = new LinkedHashMap<String, Object>();
+        storeCateInfoRepo.save(data);
+        map.put("status", true);
+        map.put("message", "가게 카테고리 정보가 추가 되었습니다.");
+        map.put("code", HttpStatus.CREATED);
+        return map;
+    }
+
+    // 가게 검색 기능
+    // Test1
+    public Map<String, Object> getStoreList(Pageable pageable, String keyword) {
+        Map<String, Object> resultMap = new LinkedHashMap<String, Object>();
+        if (keyword == null) {
+            keyword = "";
+        }
+        Page<StoreInfoEntity> page = sInfoRepo.findBySiName(pageable, keyword);
+        resultMap.put("keyword", keyword);
+        resultMap.put("totalPage", page.getTotalPages());
+        resultMap.put("currentPage", page.getNumber() + 1);
+        resultMap.put("totalCount", page.getTotalElements());
+        resultMap.put("list", page.getContent());
+        return resultMap;
     }
 }
