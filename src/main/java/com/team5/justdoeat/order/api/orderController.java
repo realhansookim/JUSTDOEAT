892d4050b972d64e898e8602ae556c4c43d6aec3
affkdy <affkdy@gmail.com>
2023-02-02 17:19:32 +0900

    주문내역조회

--- src/main/java/com/team5/justdoeat/order/api/orderController.java
+++ src/main/java/com/team5/justdoeat/order/api/orderController.java
@@ -2,17 +2,24 @@ package com.team5.justdoeat.order.api;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.team5.justdoeat.order.entity.OrderInfoEntity;
+import com.team5.justdoeat.order.repository.OrderInfoRepository;
 import com.team5.justdoeat.order.service.orderService;
 import com.team5.justdoeat.order.vo.OrderInfoVO;
+import com.team5.justdoeat.user.entity.UserInfoEntity;
+import com.team5.justdoeat.user.repository.UserInfoRepository;
 
 @RequestMapping("/order")
 @RestController
@@ -24,8 +31,17 @@ public class orderController {
   public ResponseEntity<Object> postAddOrder(@RequestBody OrderInfoVO orderInfoVO){
     Map<String, Object> resultMap = new LinkedHashMap<String, Object>();
     resultMap = orderService.addOrderInfo(orderInfoVO);
-    
     return new ResponseEntity<>(resultMap, HttpStatus.OK);
-    
+  }
+
+  @Autowired UserInfoRepository uRepo;
+  @Autowired OrderInfoRepository repo;
+  @GetMapping("/list")
+  public Map<String, Object> listOrder(@RequestParam Long userNo) {
+    Map<String, Object> resultMap = new LinkedHashMap<String, Object>();
+    UserInfoEntity entity = uRepo.findByUiSeq(userNo);
+    List<OrderInfoEntity> result = repo.findByUserInfoEntity(entity);
+    resultMap.put("list", result);
+    return resultMap;
   }
 }
